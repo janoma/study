6ee7cad0c1abcc34dbc7b6f17b3334319df9da7f
Alejandro Mallea <janoma@gmail.com>
2015-05-08 21:46:42 -0400

    Uses vector instead of a C array.

--- cache/cache.cpp
+++ cache/cache.cpp
@@ -1,29 +1,19 @@
 #include <cstdlib>
 #include <iostream>
-#include <new>
+#include <vector>
 
 #include <boost/timer/timer.hpp>
 
-void fill(int* arr, size_t size)
+void measure(std::vector<int>& arr, size_t step)
 {
-    for (size_t i = 0; i < size; ++i)
-    {
-        arr[i] = 0;
-    }
-}
-
-void measure(int* arr, size_t size, size_t step)
-{
-    std::cout << size << "," << step << "," << std::flush;
+    std::cout << arr.size() << "," << step << "," << std::flush;
 
     boost::timer::auto_cpu_timer timer("%w\n");
 
-    for (size_t i = 0; i < size; i += step)
+    for (size_t i(0), size(arr.size()); i < size; i += step)
     {
         arr[i]++;
     }
-
-    std::cout << std::flush;
 }
 
 int main(int argc, char** argv)
@@ -36,15 +26,11 @@ int main(int argc, char** argv)
 
     size_t const size = atoi(argv[1]);
 
-    int* arr = new int[size];
-
-    fill(arr, size);
+    std::vector<int> arr(size, 0);
 
     std::cout << "array size,step,time (seconds)" << std::endl;
     for (size_t step = 1; step <= 16; ++step)
     {
-        measure(arr, size, step);
+        measure(arr, step);
     }
-
-    delete[] arr;
 }
